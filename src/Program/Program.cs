abe75fae5da1ced7b23a64148d9c31ad94cbf959
Remat2 <127800134+Remat2@users.noreply.github.com>
2023-04-12 20:46:56 -0300

    Add files via upload

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,14 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("START");
+            Game newGame = new Game(FileManager.readBoard());
+            PrintBoard newPrinter = new PrintBoard();
+            while (true)
+            {
+                newGame.refreshBoard();
+                newPrinter.imprimir(newGame);
+            }
         }
     }
 }
